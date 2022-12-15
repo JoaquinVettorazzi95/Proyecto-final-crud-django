61ece457f08c1b9c02759c47517fe3459a866b01
Joaquin Vettorazzi <j.vettorazzi95@gmail.com>
2022-12-15 19:28:42 -0300

    Agregando una funcion

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ejemplo.apps.EjemploConfig',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
